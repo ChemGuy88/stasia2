a7f3f78f54440193350eb8293ee9a7b9999b13df
Herman F. Autore <herman@ufl.edu>
2023-09-16 21:06:31 -0400

    First commit

---
+++ code/RenameMe.py
@@ -0,0 +1,109 @@
+"""
+This is a template Python script.
+"""
+
+import logging
+import os
+from pathlib import Path
+# Third-party packages
+import pandas as pd
+# Local packages
+from drapi.drapi import getTimestamp, successiveParents, makeDirPath
+
+# Arguments
+_ = None
+
+# Arguments: Meta-variables
+PROJECT_DIR_DEPTH = 2
+IRB_DIR_DEPTH = PROJECT_DIR_DEPTH + 1
+IDR_DATA_REQUEST_DIR_DEPTH = IRB_DIR_DEPTH + 3
+
+LOG_LEVEL = "INFO"
+
+# Arguments: SQL connection settings
+SERVER = "DWSRSRCH01.shands.ufl.edu"
+DATABASE = "DWS_PROD"
+USERDOMAIN = "UFAD"
+USERNAME = os.environ["USER"]
+UID = None
+PWD = os.environ["HFA_UFADPWD"]
+
+# Variables: Path construction: General
+runTimestamp = getTimestamp()
+thisFilePath = Path(__file__)
+thisFileStem = thisFilePath.stem
+projectDir, _ = successiveParents(thisFilePath.absolute(), PROJECT_DIR_DEPTH)
+IRBDir, _ = successiveParents(thisFilePath, IRB_DIR_DEPTH)
+IDRDataRequestDir, _ = successiveParents(thisFilePath.absolute(), IDR_DATA_REQUEST_DIR_DEPTH)
+dataDir = projectDir.joinpath("data")
+if dataDir:
+    inputDataDir = dataDir.joinpath("input")
+    outputDataDir = dataDir.joinpath("output")
+    if outputDataDir:
+        runOutputDir = outputDataDir.joinpath(thisFileStem, runTimestamp)
+logsDir = projectDir.joinpath("logs")
+if logsDir:
+    runLogsDir = logsDir.joinpath(thisFileStem)
+sqlDir = projectDir.joinpath("sql")
+
+# Variables: Path construction: Project-specific
+pass
+
+# Variables: SQL Parameters
+if UID:
+    uid = UID[:]
+else:
+    uid = fr"{USERDOMAIN}\{USERNAME}"
+conStr = f"mssql+pymssql://{uid}:{PWD}@{SERVER}/{DATABASE}"
+
+# Variables: Other
+pass
+
+# Directory creation: General
+makeDirPath(runOutputDir)
+makeDirPath(runLogsDir)
+
+# Directory creation: Project-specific
+pass
+
+
+if __name__ == "__main__":
+    # Logging block
+    logpath = runLogsDir.joinpath(f"log {runTimestamp}.log")
+    fileHandler = logging.FileHandler(logpath)
+    fileHandler.setLevel(LOG_LEVEL)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setLevel(LOG_LEVEL)
+
+    logging.basicConfig(format="[%(asctime)s][%(levelname)s](%(funcName)s): %(message)s",
+                        handlers=[fileHandler, streamHandler],
+                        level=LOG_LEVEL)
+
+    logging.info(f"""Begin running "{thisFilePath}".""")
+    logging.info(f"""All other paths will be reported in debugging relative to `projectDir`: "{projectDir}".""")
+    logging.info(f"""Script arguments:
+
+    # Arguments
+    ``: "{"..."}"
+
+    # Arguments: General
+    `PROJECT_DIR_DEPTH`: "{PROJECT_DIR_DEPTH}"
+    `IRB_DIR_DEPTH`: "{IRB_DIR_DEPTH}"
+    `IDR_DATA_REQUEST_DIR_DEPTH`: "{IDR_DATA_REQUEST_DIR_DEPTH}"
+
+    `LOG_LEVEL` = "{LOG_LEVEL}"
+
+    # Arguments: SQL connection settings
+    `SERVER` = "{SERVER}"
+    `DATABASE` = "{DATABASE}"
+    `USERDOMAIN` = "{USERDOMAIN}"
+    `USERNAME` = "{USERNAME}"
+    `UID` = "{UID}"
+    `PWD` = censored
+    """)
+
+    # Script
+    _ = pd
+
+    # End script
+    logging.info(f"""Finished running "{thisFilePath.relative_to(projectDir)}".""")
